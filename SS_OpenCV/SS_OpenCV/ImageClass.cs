6a2772f9cf1a25ae5ba35af1cb8e299dbd54ab84
João Barata Oliveira <jmdbo@hotmail.com>
2015-09-14 15:27:36 +0100

    Initial commit

---
+++ SS_OpenCV/SS_OpenCV/ImageClass.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Emgu.CV.Structure;
+using Emgu.CV;
+
+namespace SS_OpenCV
+{
+    class ImageClass
+    {
+
+        /// <summary>
+        /// Negativo da Imagem
+        /// Manipulação Imagem - Primitivas EmguCV
+        /// Algoritmo de manipulação de imagem mais lento 
+        /// </summary>
+        /// <param name="img">Imagem</param>
+        internal static void Negative(Image<Bgr, byte> img)
+        {
+            Bgr aux;
+            for (int y = 0; y < img.Height; y++)
+            {
+                for (int x = 0; x < img.Width; x++)
+                {
+                    // acesso directo : mais lento 
+                    aux = img[y, x];
+                    img[y, x] = new Bgr(255 - aux.Blue, 255 - aux.Green, 255 - aux.Red);
+                }
+            }
+        }
+     
+
+        /// <summary>
+        /// Conversão para Cinzento
+        /// Manipulação Imagem - Acesso directo à memoria
+        /// </summary>
+        /// <param name="img">imagem</param>
+        /// <param name="imgUndo">Cópia da Imagem</param>
+        internal static void ConvertToGray(Image<Bgr, byte> img)
+        {
+            unsafe
+            {
+                // acesso directo à memoria da imagem (sequencial)
+                // direcção top left -> bottom right
+
+                MIplImage m = img.MIplImage;
+                byte* dataPtr = (byte*)m.imageData.ToPointer(); // obter apontador do inicio da imagem
+                byte blue, green, red, gray;
+
+                int width = img.Width;
+                int height = img.Height;
+                int nChan = m.nChannels; // numero de canais 3
+                int padding = m.widthStep - m.nChannels * m.width; // alinhamento (padding)
+                int x, y;
+                        
+                if (nChan == 3) // imagem em RGB
+                {
+                    for (y = 0; y < height; y++)
+                    {
+                        for (x = 0; x < width; x++)
+                        {
+                            //obtém as 3 componentes
+                            blue = dataPtr[0];
+                            green = dataPtr[1];
+                            red = dataPtr[2];
+
+                            // converte para cinza
+                            gray = (byte)(((int)blue + green + red) / 3);
+
+                            // guarda na imagem
+                            dataPtr[0] = gray;
+                            dataPtr[1] = gray;
+                            dataPtr[2] = gray;
+
+                            // avança apontador para próximo pixel
+                            dataPtr += nChan;
+                        }
+
+                        //no fim da linha avança alinhamento (padding)
+                        dataPtr += padding;
+                    }
+                }
+            }
+        }
+
+
+    }
+}
---
+++ SS_OpenCV/SS_OpenCV/MainForm.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.UI;
+using Emgu.CV.Structure;
+
+namespace SS_OpenCV
+{
+    public partial class MainForm : Form
+    {
+        Image<Bgr, Byte> img = null; // imagem corrente
+        Image<Bgr, Byte> imgUndo = null; // imagem backup - UNDO
+        string title_bak = "";
+
+        public MainForm()
+        {
+            InitializeComponent();
+            title_bak = Text;
+        }
+
+        /// <summary>
+        /// Abrir uma nova imagem
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                img = new Image<Bgr, byte>(openFileDialog1.FileName);
+                Text = title_bak + " [" +
+                        openFileDialog1.FileName.Substring(openFileDialog1.FileName.LastIndexOf("\\") + 1) +
+                        "]";
+                imgUndo = img.Copy();
+                ImageViewer.Image = img.Bitmap;
+                ImageViewer.Refresh();
+            }
+        }
+
+        /// <summary>
+        /// Guardar a imagem com novo nome
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                ImageViewer.Image.Save(saveFileDialog1.FileName);
+            }
+        }
+
+        /// <summary>
+        /// Fecha a aplicação
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        /// <summary>
+        /// repoe a ultima copia da imagem
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void undoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (imgUndo == null) // protege de executar a função sem ainda ter aberto a imagem 
+                return; 
+            Cursor = Cursors.WaitCursor;
+            img = imgUndo.Copy();
+            ImageViewer.Image = img.Bitmap;
+            ImageViewer.Refresh();
+            Cursor = Cursors.Default;
+        }
+
+        /// <summary>
+        /// Altera o modo de vizualização
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void autoZoomToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // zoom
+            if (autoZoomToolStripMenuItem.Checked)
+            {
+                ImageViewer.SizeMode = PictureBoxSizeMode.Zoom;
+                ImageViewer.Dock = DockStyle.Fill;
+            }
+            else // com scroll bars
+            {
+                ImageViewer.Dock = DockStyle.None;
+                ImageViewer.SizeMode = PictureBoxSizeMode.AutoSize;
+            }
+        }
+
+        /// <summary>
+        /// Mostra a janela Autores
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void autoresToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AuthorsForm form = new AuthorsForm();
+            form.ShowDialog();
+        }
+
+
+        /// <summary>
+        /// Converte a imagem para tons de cinzento
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void grayToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (img == null) // protege de executar a função sem ainda ter aberto a imagem 
+                return;
+            Cursor = Cursors.WaitCursor; // cursor relogio
+
+            //copy Undo Image
+            imgUndo = img.Copy();
+
+            ImageClass.ConvertToGray(img);
+
+            ImageViewer.Refresh(); // atualiza imagem no ecrã
+
+            Cursor = Cursors.Default; // cursor normal
+        }
+
+        /// <summary>
+        /// Efectua o negativo da imagem
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void negativeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (img == null) // protege de executar a função sem ainda ter aberto a imagem 
+                return;
+            Cursor = Cursors.WaitCursor; // cursor relogio
+
+            //copy Undo Image
+            imgUndo = img.Copy();
+
+            ImageClass.Negative(img);
+
+            ImageViewer.Refresh(); // atualiza imagem no ecrã
+
+            Cursor = Cursors.Default; // cursor normal
+        }
+
+
+
+
+    }
+}
